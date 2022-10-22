216c332cacd7e228b17617cc1470f835a43f884c
Andrew Tinsley <andtinsley@gmail.com>
2022-10-21 20:56:28 -0700

    Concatenate output values
    updated to make data long. Added script to concatenate long data per each row for analysis on matlab

--- fUS_Correlation_Matrix_Parser.py
+++ fUS_Correlation_Matrix_Parser.py
@@ -1,6 +1,9 @@
+
+
 import pandas as pd
 import os
 import glob
+import sys
 import numpy as np
 import re
 
@@ -11,13 +14,21 @@ import re
 ##    b) Z-Score from correlation coefficient
 ##    c) Updated Correlation coefficient from an averaged z-score
 # getting csv files from the folder MyProject
+
+#os.chdir(os.path.dirname(__file__))
+#path = os.getcwd()
+
+
+
+# determine if application is a script file or frozen exe
 path = os.getcwd()
 
 # read all the files with extension .csv
-filenames = glob.glob(path + "\*.txt")
+filenames = glob.glob(os.path.join(path, "*.txt"))
 newpath = path+'/Extract'
 if not os.path.exists(newpath):
    os.makedirs(newpath)
+
 
 d_list = []
 z_list = []
@@ -27,11 +38,11 @@ for file in filenames:
    # reading csv files
    d = pd.read_csv(file, sep=';')
    
+   
    #drop duplicated columns: columns with period present
    d.drop([col for col in d.columns if '.' in col],axis=1, inplace=True)
    #drop duplicated rows
    d = d.drop_duplicates(subset=['Matrix'])
-
    file_name = file.rsplit(' ', 1)[1]
    file_name = file_name.rsplit('.', 1)[0]   
    #d.to_excel(newpath+"\\"+file_name + ".xlsx", index = False)  
@@ -45,12 +56,10 @@ for file in filenames:
    d_z_score = (1+d_indexed[numeric_cols]) / (1-d_indexed[numeric_cols])
    d_z_score = np.log10(d_z_score) / 2
    d_z_score.reset_index(drop=False, inplace=True)
-
    #Export
    # create a excel writer object
-   with pd.ExcelWriter(newpath+"\\"+file_name + ".xlsx") as writer:
-      d.to_excel(writer, sheet_name="Extract", index=False)
-      d_z_score.to_excel(writer, sheet_name="Z Scores", index=False)
+   d.to_csv(newpath+"\\"+file_name +".csv", index=False)
+   d_z_score.to_csv(newpath+"\\"+"Z Scores_"+file_name+ ".csv", index=False)
 
    d_list.append(d)
    z_list.append(d_z_score)
@@ -63,6 +72,14 @@ average_z_score = z_score.groupby('Matrix').mean()
 #count of occurences
 n_matrix = df_final.groupby('Matrix').count()
 
+#transpose data for analysis
+make_long=average_z_score.stack().reset_index()
+make_long['corr_features'] = make_long['Matrix'] +' x ' + make_long['level_1']
+make_long = make_long[['corr_features', 0]]
+make_long = make_long.set_index('corr_features').T.reset_index()
+make_long = make_long.iloc[: , 1:]
+
+
 #export 
 #file name
 updated_file_name = file_name.split('_', 1)[-1]
@@ -72,6 +89,13 @@ nextpath = path+'/Calculated'
 if not os.path.exists(nextpath):
    os.makedirs(nextpath)
 # create a excel writer object
-with pd.ExcelWriter(nextpath+"\\"+updated_file_name + ".xlsx") as writer:
-   average_z_score.to_excel(writer, sheet_name="Average Z", index=True)
-   n_matrix.to_excel(writer, sheet_name="Samples", index=True)
+
+
+#with pd.ExcelWriter(nextpath+"\\"+updated_file_name + ".xlsx") as writer:
+#  average_z_score.to_excel(writer, sheet_name="Average Z", index=True)
+  # n_matrix.to_excel(writer, sheet_name="Samples", index=True)
+   #make_long.to_excel(writer, sheet_name="Avg Z Trans", index=False)
+
+average_z_score.to_csv(nextpath+"\\"+ "Average_Z_Score_"  + updated_file_name +".csv", index=False)
+n_matrix.to_csv(nextpath+"\\"+ "Samples_"  + updated_file_name +".csv", index=False)
+make_long.to_csv(nextpath+"\\"+ "Long_"  + updated_file_name +".csv", index=False)
